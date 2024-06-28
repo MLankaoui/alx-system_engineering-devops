ec16675be2b01eef46fc6f6ce9a443d8e33eca44
root <root@DESKTOP-SQO21O6.localdomain>
2024-06-28 16:31:37 +0100

    task 1 another attempt

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # insalling flask from Puppet
 
-package {'flask':
-    ensure  => 'installed',
-    version => '2.1.0',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0 Werkzeug==2.1.1',
+  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
 }
